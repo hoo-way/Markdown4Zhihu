8afbbf0aa45caad28f3ad00e2dcc15b1381a846b
silabs-victorh <victor.hu@silabs.com>
2020-12-22 09:50:23 +0000

    update file README

--- zhihu-publisher.py
+++ zhihu-publisher.py
@@ -49,6 +49,7 @@ def formula_ops(_lines):
 def rename_image_ref(m, original=True):
     global image_folder_path
     # image_folder_path
+    print(m.group(2) )
     print(os.path.basename (m.group(2) ))
     # print(image_folder_path.name)
     # if not Path(m.group(1)).is_file():
